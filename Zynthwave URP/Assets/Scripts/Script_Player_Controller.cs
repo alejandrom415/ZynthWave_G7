7d73b14ca98ebd80094a4ff52b9175964192ebb3
alejandrom415 <alejandrom415@knights.ucf.edu>
2022-09-04 12:12:29 -0400

    begun work on wave spawner

---
+++ Zynthwave URP/Assets/Scripts/Script_Player_Controller.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Player_Controller : MonoBehaviour
+{
+    [System Serializable]
+
+    public class Wave
+    {
+        public string name;
+
+        public Transform enemy;
+
+        public int count;
+
+        public float rate;
+    }
+    
+    public Wave[] waves;
+
+    private int nextWave = 0;
+
+    public float timeBetweenWaves = 15f;
+
+    public float waveCountdown;
+
+    void Start()
+    {
+        waveCountdown = timeBetweenWaves;
+    }
+
+    void Update()
+    {
+        if (waveCountdown <= 0)
+        {
+            
+        }
+    }
+}
